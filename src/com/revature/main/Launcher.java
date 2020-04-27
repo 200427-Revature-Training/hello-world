91a6030bea9f1debbb892d140f5f5530d50cf8c9
Mitch Goshorn <msgoshorn@gmail.com>
2020-04-27 14:54:11 -0400

    Initial commit on hello-world, simple Java app

---
+++ src/com/revature/main/Launcher.java
@@ -0,0 +1,30 @@
+package com.revature.main;
+
+/**
+ * Every class file should have a single top-level public class
+ * with the same name as the .java file.
+ */
+public class Launcher {
+
+	/**
+	 * -- Doc-Comments --
+	 * Every Java application begins by executing the main method.
+	 * The main the method itself represents the starting point 
+	 * and its execution often is the entire lifecycle of the 
+	 * application.
+	 * 
+	 * The main method in Java has a very specific signature
+	 * 	(Signature describes the keywords associated with a method
+	 * 		declaration)
+	 * 
+	 * @param args - Command line arguments provided at application execution
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Comment block
+		 */
+		
+		// When run, code will begin executing here
+		System.out.println("Hello World!");
+	}
+}
